c999c1fdb8a9c4318c85b9ad7d2969ad3bf9aa2f
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2021-10-25 11:17:51 -0400

    updated visualizations to include geo viz colormap by mse and regular mse for model 2 training curves; better results saving

--- train.py
+++ train.py
@@ -7,7 +7,8 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from torch.optim import lr_scheduler
-import math
+from datetime import datetime
+import os
 
 from tandem import Forward, Backward
 import data_reader
@@ -30,11 +31,9 @@ def boundary_loss(x):
     total_loss = relu(mean_diff)
     return torch.mean(total_loss)
 
-def loss(ys, labels, x = None, prev = None):
+def loss(ys, labels, x = None, prev = None, sigma = 0, l = 0):
     bdy = 0
     prev_loss = 0
-    sigma = 0.5
-    l = 0.1
     if x is not None:
         bdy = boundary_loss(x)
     if prev is not None:
@@ -43,12 +42,14 @@ def loss(ys, labels, x = None, prev = None):
 
     return bdy + mse - prev_loss
 
-def train():
+def train(dir):
     train_data, test_data = data_reader.read_data_sine_wave()
 
     num_epochs = 500
     #num_epochs = 40
     #num_epochs = 5
+    sigma = 0.5
+    lam = 0.1
 
     model_f = Forward()
     model_b = Backward()
@@ -150,6 +151,7 @@ def train():
 
     #Backward model 2
     backward_train_losses2, backward_eval_losses2 = [], []
+    backward_train_losses2_mses, backward_eval_losses2_mses = [], []
     print("Training second backwards model")
     model_f.eval()
     model_b.eval()
@@ -160,6 +162,7 @@ def train():
         model_b2.train()
         model_b.eval()
         epoch_losses = []
+        epoch_losses_mses = []
         for i, (g, s) in enumerate(train_data):
             if cuda:
                 g.cuda()
@@ -168,17 +171,22 @@ def train():
             opt_b2.zero_grad()
             g_out = model_b2(s)
             s_out = model_f(g_out)
-            l = loss(s_out, s, x=g_out, prev=model_b(s))
+            l = loss(s_out, s, x=g_out, prev=model_b(s), sigma=sigma, l=lam)
+            l2 = loss(s_out, s, x=g_out)
             l.backward()
             opt_b2.step()
             epoch_losses.append(l.detach().numpy())
+            epoch_losses_mses.append(l2.detach().numpy())
         epoch_loss = np.mean(epoch_losses)
+        epoch_loss_mse = np.mean(epoch_losses_mses)
         backward_train_losses2.append(epoch_loss)
+        backward_train_losses2_mses.append(epoch_loss_mse)
         lr_sched.step(epoch_loss)
         if epoch % 20 == 0:
             model_b2.eval()
             print("Eval epoch " + str(epoch))
             eval_epoch_losses = []
+            eval_epoch_losses_mse = []
             for i, (g, s) in enumerate(test_data):
                 if cuda:
                     g.cuda()
@@ -186,10 +194,14 @@ def train():
 
                 g_out = model_b2(s)
                 s_out = model_f(g_out)
-                l = loss(s_out, s, x=g_out, prev=model_b(s))
+                l = loss(s_out, s, x=g_out, prev=model_b(s), sigma=sigma, l=lam)
+                l2 = loss(s_out, s, x=g_out)
                 eval_epoch_losses.append(l.detach().numpy())
+                eval_epoch_losses_mse.append(l2.detach().numpy())
             epoch_loss = np.mean(eval_epoch_losses)
+            epoch_loss_mse = np.mean(eval_epoch_losses_mse)
             backward_eval_losses2.append(epoch_loss)
+            backward_eval_losses2_mses.append(epoch_loss_mse)
             print("Backwards 2 train loss on epoch {}: {}".format(epoch, backward_train_losses2[-1]))
             print("Backwards 2 eval loss on epoch {}: {}".format(epoch, epoch_loss))
 
@@ -262,8 +274,11 @@ def train():
         sim_mses.append(best_sim_err)
 
     #results
-    results = open("results.txt", "w")
-
+    results = open("{}/results.txt".format(dir), "w")
+    results.write("Number of epochs: {}\n".format(num_epochs))
+    results.write("Sigma: {}\n".format(sigma))
+    results.write("Sigma squared: {}\n".format(sigma ** 2))
+    results.write("Lambda: {}\n".format(lam))
     x = [np.log(fwd_mses), np.log(sim_mses)]
     x1 = [np.log(model1_fwd_mses), np.log(model1_sim_mses)]
     x2 = [np.log(model2_fwd_mses), np.log(model2_sim_mses)]
@@ -284,7 +299,7 @@ def train():
     plt.ylabel("Count")
     plt.hist(x, bins=100, label=['forward', 'simulator'])
     plt.legend()
-    plt.savefig("histogram_best_model.png")
+    plt.savefig("{}/histogram_best_model.png".format(dir))
 
     plt.figure(2)
     plt.title("Error histogram for inverse model 1")
@@ -292,7 +307,7 @@ def train():
     plt.ylabel("Count")
     plt.hist(x1, bins=100, label=['forward', 'simulator'])
     plt.legend()
-    plt.savefig("histogram_model1.png")
+    plt.savefig("{}/histogram_model1.png".format(dir))
 
     plt.figure(3)
     plt.title("Error histogram for inverse model 2")
@@ -300,65 +315,91 @@ def train():
     plt.ylabel("Count")
     plt.hist(x2, bins=100, label=['forward', 'simulator'])
     plt.legend()
-    plt.savefig("histogram_model2.png")
+    plt.savefig("{}/histogram_model2.png".format(dir))
 
     #geometry visualization
-    test_s = np.linspace(-1, 1, 500)
+    test_s = np.linspace(-1, 1, 100)
     test_g = model_b(torch.tensor(test_s, dtype=torch.float).unsqueeze(1)).detach().numpy()
     test_g2 = model_b2(torch.tensor(test_s, dtype=torch.float).unsqueeze(1)).detach().numpy()
 
+    true_s = np.sin(3 * np.pi * test_g[:, 0]) + np.cos(3 * np.pi * test_g[:, 1])
+    true_s2 = np.sin(3 * np.pi * test_g2[:, 0]) + np.cos(3 * np.pi * test_g2[:, 1])
+
+    test_err = (true_s - test_s)**2
+    test_err2 = (true_s2 - test_s) ** 2
+
     plt.figure(4)
     plt.title("Visualization of output geometries")
-    plt.plot(test_g[:, 0], test_g[:, 1], label='Inverse Model 1')
-    plt.plot(test_g2[:, 0], test_g2[:, 1], label='Inverse Model 2')
+    plt.scatter(test_g[:, 0], test_g[:, 1], s=10, label='Inverse Model 1')
+    plt.scatter(test_g2[:, 0], test_g2[:, 1], s=10, label='Inverse Model 2')
     plt.legend()
-    plt.savefig("geometry_visualization.png")
+    plt.savefig("{}/geometry_visualization.png".format(dir))
+
+    plt.figure(5)
+    plt.title("Visualization of inverse model 1 output geometries")
+    plt.scatter(test_g[:, 0], test_g[:, 1], c=test_err, cmap='plasma')
+    plt.colorbar(label='MSE', orientation='horizontal')
+    plt.savefig("{}/geometry_visualization_model1_mse".format(dir))
+
+    plt.figure(6)
+    plt.title("Visualization of inverse model 2 output geometries")
+    plt.scatter(test_g2[:, 0], test_g2[:, 1], c=test_err2, cmap='plasma')
+    plt.colorbar(label='MSE', orientation='horizontal')
+    plt.savefig("{}/geometry_visualization_model2_mse".format(dir))
 
     #training graphs
-    plt.figure(5)
+    plt.figure(7)
     plt.title("Forward training error")
     results.write("Forward training error {:0.4f}\n".format(min(forward_train_losses)))
     plt.plot(range(num_epochs), forward_train_losses)
     plt.xlabel('Epochs')
     plt.ylabel('Train Error')
     plt.yscale("log")
-    plt.savefig("forward_loss_train.png")
+    plt.savefig("{}/forward_loss_train.png".format(dir))
 
-    plt.figure(6)
+    plt.figure(8)
     plt.title("Forward eval error")
     results.write("Forward eval error {:0.4f}\n".format(min(forward_eval_losses)))
     plt.plot(range(0, num_epochs, 20), forward_eval_losses)
     plt.xlabel('Epochs')
     plt.ylabel('Eval Error')
     plt.yscale("log")
-    plt.savefig("forward_loss_eval.png")
+    plt.savefig("{}/forward_loss_eval.png".format(dir))
 
-    plt.figure(7)
+    plt.figure(9)
     plt.title("Backward training error")
     results.write("Backward model 1 training error {:0.4f}\n".format(min(backward_train_losses)))
     results.write("Backward model 2 training error {:0.4f}\n".format(min(backward_train_losses2)))
+    results.write("Backward model 2 training error without repulsion term {:0.4f}\n".format(min(backward_train_losses2_mses)))
     plt.plot(range(num_epochs), backward_train_losses, label = 'Inverse Model 1')
     plt.plot(range(num_epochs), backward_train_losses2, label='Inverse Model 2')
+    plt.plot(range(num_epochs), backward_train_losses2_mses, label='Inverse Model 2 without repulsion term')
     plt.legend()
     plt.xlabel('Epochs')
     plt.ylabel('Train Error')
     plt.yscale("log")
-    plt.savefig("backward_loss_train.png")
+    plt.savefig("{}/backward_loss_train.png".format(dir))
 
-    plt.figure(8)
+    plt.figure(10)
     plt.title("Backward eval error")
     results.write("Backward model 1 eval error {:0.4f}\n".format(min(backward_eval_losses)))
     results.write("Backward model 2 eval error {:0.4f}\n".format(min(backward_eval_losses2)))
+    results.write(
+        "Backward model 2 eval error without repulsion term {:0.4f}\n".format(min(backward_eval_losses2_mses)))
     plt.plot(range(0, num_epochs, 20), backward_eval_losses, label = 'Inverse Model 1')
     plt.plot(range(0, num_epochs, 20), backward_eval_losses2, label='Inverse Model 2')
+    plt.plot(range(0, num_epochs, 20), backward_eval_losses2_mses, label='Inverse Model 2 without repulsion term')
     plt.legend()
     plt.xlabel('Epochs')
     plt.ylabel('Test Error')
     plt.yscale("log")
-    plt.savefig("backward_loss_eval.png")
+    plt.savefig("{}/backward_loss_eval.png".format(dir))
 
     results.close()
 if __name__ == '__main__':
     print("Beginning training")
-    train()
+    x = datetime.now()
+    dir = "{:04d}-{:02d}-{:02d}_{:02d}-{:02d}".format(x.year, x.month, x.day, x.hour, x.minute)
+    os.mkdir(dir)
+    train(dir)
     print("Done training")
